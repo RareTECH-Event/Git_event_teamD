ba4c61e08e65ec28fa9497057e9e14d92baa37fc
yuhencm <ucohanda@gmail.com>
2025-04-16 08:09:29 +0700

    3回目コミット

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ def main():
         print("1: メンバー1の名前")
         print("2: ゆうすけ")
         print("3: ヨウズ")
+        print("4: よぅずver.2.0")
         print("q: 終了")
 
         choice = input("> ")
@@ -14,6 +15,8 @@ def main():
             print("初心者です")
         elif choice == "3":
             print("今日はみなさんありがとうございました")
+        elif choice == "4":
+            print("早起きは三文の得。でも、三文っていくら？")
         elif choice == "q":
             print("プログラムを終了します。")
             break
